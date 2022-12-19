8ed6ca294934c4b8b58d75f39bcc9a51b261b864
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2022-12-18 22:55:15 -0500

    Fixed `Std.isOfType`, `Std.downcast`, and `Std.parseFloat`

--- std/gdscript/_std/Std.hx
+++ std/gdscript/_std/Std.hx
@@ -3,13 +3,15 @@ package;
 extern class Std {
 	@:deprecated('Std.is is deprecated. Use Std.isOfType instead.')
 	public extern inline static function is(v: Dynamic, t: Dynamic): Bool return isOfType(v, t);
-	public extern inline static function isOfType(v: Dynamic, t: Dynamic): Bool {
-		return untyped __gdscript__("{}.get_class() == {}.get_class_name()", v, t);
-	}
+
+	@:nativeFunctionCode("({arg0} is {arg1})")
+	public static function isOfType(v: Dynamic, t: Dynamic): Bool;
 
 	@:deprecated('Std.instance() is deprecated. Use Std.downcast() instead.')
 	public extern inline static function instance<T: {}, S: T>(value: T, c: Class<S>): S return downcast(value, c);
-	public extern inline static function downcast<T: {}, S: T>(value: T, c: Class<S>): S return cast value;
+
+	@:nativeFunctionCode("({arg0} as {arg1})")
+	public static function downcast<T: {}, S: T>(value: T, c: Class<S>): S;
 
 	@:native("str")
 	public static function string(s: Dynamic): String;
@@ -20,7 +22,7 @@ extern class Std {
 	@:nativeFunctionCode("{arg0}.to_int()")
 	public static function parseInt(x: String): Null<Int>;
 
-	@:nativeFunctionCode("{arg0}.to_int()")
+	@:nativeFunctionCode("{arg0}.to_float()")
 	public  static function parseFloat(x: String): Float;
 
 	@:nativeFunctionCode("floor(randf() * {arg0})")
