c0635b74178c495cecd9d99b9e86279259d26c05
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-11-04 03:21:36 -0400

    Finish Run script to generate bindings

--- Run.hx
+++ Run.hx
@@ -4,17 +4,21 @@ import sys.FileSystem;
 import sys.io.Process;
 
 function main() {
-	final libPath = switch(Sys.args()) {
-		case [libPath]: {
-			libPath;
+	// Ensure we use current directory of haxelib run call.
+	final cwd = switch(Sys.args()) {
+		case [cwd]: {
+			cwd;
 		}
 		case args: {
 			args[args.length - 1];
 		}
 	}
+	Sys.setCwd(cwd);
 
+	// We start!!
 	Sys.println("Generating Godot bindings...");
 
+	// Check for godot executable
 	var godotVersion = "";
 	var godotPath: Null<String> = Sys.getEnv("GODOT_PATH");
 	if(godotPath == null) {
@@ -30,6 +34,7 @@ function main() {
 		}
 	}
 
+	// Request path to godot executable if not found
 	if(godotPath == null) {
 		Sys.println("'godot' could not be found, please enter the path to the executable:");
 		Sys.print("> ");
@@ -63,10 +68,31 @@ function main() {
 		}
 	}
 
+	// Generate `extension_api.json`
+	Sys.println('>> ${godotPath} --dump-extension-api-with-docs --headless');
 	final process = new Process(godotPath, ["--dump-extension-api-with-docs", "--headless"]);
 	process.exitCode(true);
 
-	// TODO: Generate bindings from extension_api.json
+	// Install `godot-api-generator`
+	Sys.println('>> haxelib install godot-api-generator');
+	final process = new Process("haxelib", ["install", "godot-api-generator"]);
+	if(process.exitCode(true) != 0) {
+		Sys.println("Could not install 'godot-api-generator' from haxelib.\n");
+		Sys.println(process.stdout.readAll().toString());
+		Sys.println(process.stderr.readAll().toString());
+		return;
+	}
 
+	// Generate bindings using `godot-api-generator`
+	Sys.println('>> haxelib run godot-api-generator extension_api.json godot');
+	final process = new Process("haxelib", ["run", "godot-api-generator", "./extension_api.json", "./godot"]);
+	if(process.exitCode(true) != 0) {
+		Sys.println("Could not run 'godot-api-generator'.\n");
+		Sys.println(process.stdout.readAll().toString());
+		Sys.println(process.stderr.readAll().toString());
+		return;
+	}
+
+	// We done!!
 	Sys.println("Done!");
 }
