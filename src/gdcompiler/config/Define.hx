7fac4d54cb28710a226ec39995b8dfe363535d4d
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-11-07 04:24:32 -0500

    Allow plugin entries to be modified with defines

---
+++ src/gdcompiler/config/Define.hx
@@ -0,0 +1,38 @@
+package gdcompiler.config;
+
+enum abstract Define(String) from String to String {
+	/**
+		-D generate_godot_plugin
+
+		If defined, a Godot plugin will be generated with the output.
+	**/
+	var GenerateGodotPlugin = "generate_godot_plugin";
+
+	/**
+		-D godot_plugin_name=NAME
+
+		Defines the "name" entry in `plugin.cfg` if a plugin is generated.
+	**/
+	var GodotPluginName = "godot_plugin_name";
+
+	/**
+		-D godot_plugin_description=DESCRIPTION
+
+		Defines the "description" entry in `plugin.cfg` if a plugin is generated.
+	**/
+	var GodotPluginDescription = "godot_plugin_description";
+
+	/**
+		-D godot_plugin_author=AUTHOR
+
+		Defines the "author" entry in `plugin.cfg` if a plugin is generated.
+	**/
+	var GodotPluginAuthor = "godot_plugin_author";
+
+	/**
+		-D godot_plugin_version=VERSION
+
+		Defines the "version" entry in `plugin.cfg` if a plugin is generated.
+	**/
+	var GodotPluginVersion = "godot_plugin_version";
+}
