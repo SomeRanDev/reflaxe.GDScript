41c190af0619a225c577c06fd751520b388e481d
SomeRanDev <28355157+SomeRanDev@users.noreply.github.com>
2025-03-10 19:03:40 +0000

    Build: (90fa89a) Reserve `utility_functions` after all

--- src/gdcompiler/GDCompilerInit.hx
+++ src/gdcompiler/GDCompilerInit.hx
@@ -96,12 +96,32 @@ class GDCompilerInit {
 		https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_basics.html
 	**/
 	static var _reservedNames = [
+		// keywords
 		"if", "elif", "else", "for", "while", "match", "break", "continue", "pass",
 		"return", "class", "class_name", "extends", "is", "in", "as", "self", "signal",
 		"func", "static", "const", "enum", "var", "breakpoint", "preload",
 		"await", "yield", "assert", "void", "PI", "TAU", "INF", "NAN",
 
-		"void", "bool", "int", "float", 
+		// types
+		"void", "bool", "int", "float",
+
+		// utility_functions
+		"sin", "cos", "tan", "sinh", "cosh", "tanh", "asin", "acos", "atan", "atan2",
+		"asinh", "acosh", "atanh", "sqrt", "fmod", "fposmod", "posmod", "floor", "floorf",
+		"floori", "ceil", "ceilf", "ceili", "round", "roundf", "roundi", "abs", "absf",
+		"absi", "sign", "signf", "signi", "snapped", "snappedf", "snappedi", "pow", "log",
+		"exp", "is_nan", "is_inf", "is_equal_approx", "is_zero_approx", "is_finite", "ease",
+		"step_decimals", "lerp", "lerpf", "cubic_interpolate", "cubic_interpolate_angle",
+		"cubic_interpolate_in_time", "cubic_interpolate_angle_in_time", "bezier_interpolate",
+		"bezier_derivative", "angle_difference", "lerp_angle", "inverse_lerp", "remap", "smoothstep",
+		"move_toward", "rotate_toward", "deg_to_rad", "rad_to_deg", "linear_to_db", "db_to_linear",
+		"wrap", "wrapi", "wrapf", "max", "maxi", "maxf", "min", "mini", "minf", "clamp", "clampi",
+		"clampf", "nearest_po2", "pingpong", "randomize", "randi", "randf", "randi_range", "randf_range",
+		"randfn", "seed", "rand_from_seed", "weakref", "typeof", "type_convert", "str", "error_string",
+		"type_string", "print", "print_rich", "printerr", "printt", "prints", "printraw", "print_verbose",
+		"push_error", "push_warning", "var_to_str", "str_to_var", "var_to_bytes", "bytes_to_var",
+		"var_to_bytes_with_objects", "bytes_to_var_with_objects", "hash", "instance_from_id", "is_instance_id_valid",
+		"is_instance_valid", "rid_allocate_id", "rid_from_int64", "is_same",
 
 		// Reserved for @:wrapper argument
 		"_self"
