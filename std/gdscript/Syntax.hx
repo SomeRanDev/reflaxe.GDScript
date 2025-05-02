1169af452f48c83875abf63570c4087d683f559b
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2025-05-02 03:46:43 -0400

    Add `Syntax.percent`

--- std/gdscript/Syntax.hx
+++ std/gdscript/Syntax.hx
@@ -1,6 +1,22 @@
 package gdscript;
 
-extern class Syntax {
+import haxe.macro.Context;
+import haxe.macro.Expr;
+
+class Syntax {
 	@:nativeFunctionCode("({arg0} is {arg1})")
-	public static function is<T, U>(any: T, class_type: Class<U>): Bool;
+	public static extern function is<T, U>(any: T, class_type: Class<U>): Bool;
+
+	public static macro function percent(identifier: Expr): Expr {
+		final identifier = switch(identifier.expr) {
+			case EConst(CString(s, _) | CIdent(s)): s;
+			case _: {
+				Context.error("Expected String or identifier expression", identifier.pos);
+				return macro {};
+			}
+		}
+
+		final identifier = "%" + identifier;
+		return macro untyped __gdscript__($v{identifier});
+	}
 }
