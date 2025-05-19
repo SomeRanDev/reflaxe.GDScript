28dfce21bf2557f6dd76ebe24bfedde8e3433e02
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2025-05-19 08:55:26 -0400

    Add `Map`

---
+++ std/gdscript/_std/haxe/ds/IntMap.hx
@@ -0,0 +1,61 @@
+package haxe.ds;
+
+class IntMap<T> implements haxe.Constraints.IMap<Int, T> {
+	var m: gdscript.Dictionary<Int, T>;
+
+	public function new(): Void {
+		m = new gdscript.Dictionary<Int, T>();
+	}
+
+	public function set(key: Int, value: T): Void {
+		m.set(key, value);
+	}
+
+	public function get(key: Int): Null<T> {
+		return if(m.has(key)) {
+			m.get(key);
+		} else {
+			null;
+		}
+	}
+
+	public function exists(key: Int): Bool {
+		return m.has(key);
+	}
+
+	public function remove(key: Int): Bool {
+		return m.erase(key);
+	}
+
+	public function keys(): Iterator<Int> {
+		return m.keys().iterator();
+	}
+
+	public function iterator(): Iterator<T> {
+		return m.values().iterator();
+	}
+
+	@:runtime public inline function keyValueIterator(): KeyValueIterator<Int, T> {
+		return new haxe.iterators.MapKeyValueIterator(this);
+	}
+
+	public function copy(): IntMap<T> {
+		final result = new IntMap<T>();
+		result.m = m.duplicate(false);
+		return result;
+	}
+
+	public function toString(): String {
+		var result = "[";
+		var first = true;
+		for(key => value in this) {
+			result += (first ? "" : ", ") + (Std.string(key) + " => " + Std.string(value));
+			if(first) first = false;
+		}
+		return result + "]";
+	}
+
+	public function clear(): Void {
+		m.clear();
+	}
+}
